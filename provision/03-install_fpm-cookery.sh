c68881c1c99a0a17329a0a6965ae07e12dabc8f7
Jan Ivar Beddari <code@beddari.net>
2014-03-05 13:22:14 +0100

    Do not reinstall fpm-cookery if present

--- provision/03-install_fpm-cookery.sh
+++ provision/03-install_fpm-cookery.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/opt/fpm-refinery/embedded/bin/gem install fpm-cookery --no-ri --no-rdoc
+if [ ! -f "${omnibus_prefix}/embedded/bin/fpm-cook" ]
+then
+    /opt/fpm-refinery/embedded/bin/gem install fpm-cookery --no-ri --no-rdoc
+fi
+
